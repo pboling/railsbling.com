84056cf3b4d92669e2d82cf63ae18fa895b58c68
Peter Boling <peter.boling@gmail.com>
2022-06-30 07:37:54 +0700

    🚀 Update deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -24,8 +24,6 @@ THEME_REPO=$HOME/themes/$THEME_NAME.git
 # We clone the repo everytime we update, rather than git pull.
 SITE_TMP=$HOME/tmp/$SITE_NAME
 THEME_TMP=$HOME/tmp/$SITE_NAME/themes/$THEME_NAME
-# rm -rf $SITE_TMP
-# rm -rf $THEME_TMP
 # Where the final, built, site will land, to be served on the  internet.
 SITE_WWW=$HOME/$SITE_DOMAIN
 
@@ -35,12 +33,10 @@ cat .git/HEAD
 git -C $THEME_REPO pull --rebase
 
 # Clone into the temporary directories.
+rm -rf $SITE_TMP
+rm -rf $THEME_TMP
 git clone $SITE_REPO $SITE_TMP
 git clone $THEME_REPO $THEME_TMP
 
 # Then build
 $FAUXROOT/usr/local/bin/hugo --theme="$THEME_NAME" -s "$SITE_TMP" -d "$SITE_WWW" -b "$SITE_URL"
-
-# Then Cleanup
-# rm -rf $SITE_TMP
-# rm -rf $THEME_TMP
